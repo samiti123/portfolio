df1c486d95cce7214ba7a957711f7fba21639e33
sami <dcil142@samiti.speedport.ip>
2020-11-11 08:35:15 +0100

    adding Home

---
+++ src/Components/Home.js
@@ -0,0 +1,43 @@
+import React from "react";
+
+function Home() {
+  return (
+    <div className="view">
+      <h1 className="view-title">My Background</h1>
+      <div className="view-description">
+        <p>
+          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Veritatis
+          dolore ea saepe non repellendus perferendis autem nulla nobis esse
+          harum.
+        </p>
+        <p>
+          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Veritatis
+          dolore ea saepe non repellendus perferendis autem nulla nobis esse
+          harum.
+        </p>
+        <p>
+          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Veritatis
+          dolore ea saepe non repellendus perferendis autem nulla nobis esse
+          harum.
+        </p>
+        <p>
+          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Veritatis
+          dolore ea saepe non repellendus perferendis autem nulla nobis esse
+          harum.
+        </p>
+        <p>
+          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Veritatis
+          dolore ea saepe non repellendus perferendis autem nulla nobis esse
+          harum.
+        </p>
+        <p>
+          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Veritatis
+          dolore ea saepe non repellendus perferendis autem nulla nobis esse
+          harum.
+        </p>
+      </div>
+    </div>
+  );
+}
+
+export default Home;
---
+++ src/State.js
@@ -0,0 +1,47 @@
+export const InitialState = [
+  {
+    id: 1,
+    title: "Javascript Breakout Game",
+    description:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Tempore aliquam suscipit hic delectus odio est culpa quas maiores? Minus ullam doloribu sit cupiditate commodi consequatur voluptate, dolorem tempora a molestias iusto? Aperiam, dolores velit quasi ad quis corporis distinctio nam tenetur placeat blanditiis animi voluptate, repellendus libero aliquam, sequi cum.",
+    image: "./images/breakout.png",
+    url: "https://yourname.github.io/...",
+    code: "https://github.com/yourname/...",
+  },
+  {
+    id: 2,
+    title: "Javascript Trading App",
+    description:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Tempore aliquam suscipit hic delectus odio est culpa quas maiores? Minus ullam doloribu sit cupiditate commodi consequatur voluptate, dolorem tempora a molestias iusto? Aperiam, dolores velit quasi ad quis corporis distinctio nam tenetur placeat blanditiis animi voluptate, repellendus libero aliquam, sequi cum.",
+    image: "./images/tradingapp.png",
+    url: "https://yourname.github.io/...",
+    code: "https://github.com/yourname/...",
+  },
+  {
+    id: 3,
+    title: "Javascript Color Generator",
+    description:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Tempore aliquam suscipit hic delectus odio est culpa quas maiores? Minus ullam doloribu sit cupiditate commodi consequatur voluptate, dolorem tempora a molestias iusto? Aperiam, dolores velit quasi ad quis corporis distinctio nam tenetur placeat blanditiis animi voluptate, repellendus libero aliquam, sequi cum.",
+    image: "./images/randomcolorgenerator.png",
+    url: "https://yourname.github.io/...",
+    code: "https://github.com/yourname/...",
+  },
+  {
+    id: 4,
+    title: "NodeJS News Blog",
+    description:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Tempore aliquam suscipit hic delectus odio est culpa quas maiores? Minus ullam doloribu sit cupiditate commodi consequatur voluptate, dolorem tempora a molestias iusto? Aperiam, dolores velit quasi ad quis corporis distinctio nam tenetur placeat blanditiis animi voluptate, repellendus libero aliquam, sequi cum.",
+    image: "./images/newsblog.png",
+    url: "https://yourname.github.io/...",
+    code: "https://github.com/yourname/...",
+  },
+  {
+    id: 5,
+    title: "NodeJS Learning App",
+    description:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Tempore aliquam suscipit hic delectus odio est culpa quas maiores? Minus ullam doloribu sit cupiditate commodi consequatur voluptate, dolorem tempora a molestias iusto? Aperiam, dolores velit quasi ad quis corporis distinctio nam tenetur placeat blanditiis animi voluptate, repellendus libero aliquam, sequi cum.",
+    image: "./images/devhub.png",
+    url: "https://yourname.github.io/...",
+    code: "https://github.com/yourname/...",
+  },
+];
